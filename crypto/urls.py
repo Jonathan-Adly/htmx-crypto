b69a7093557e3eeb4ef8fdd99f593192d409445b
Jonathan Adly <jonathanadly@Jonathans-MacBook-Air.local>
2021-11-19 10:36:36 -0500

    userstory2

--- crypto/urls.py
+++ crypto/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("currencies/<str:input_clicked>", views.currencies, name="currencies"),
+    path("transactions", views.transactions, name="transactions"),
 ]
--- crypto/views.py
+++ crypto/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from .models import Transaction
 from .forms import TransactionForm
 
 
 def home(request):
-
     form = TransactionForm()
-    transactions = Transaction.objects.filter(user=request.user)
     if request.method == "POST":
         form = TransactionForm(request.POST)
         if form.is_valid():
             form = form.save(commit=False)
             form.user = request.user
             form.save()
-            transactions = Transaction.objects.filter(user=request.user)
+            transactions = Transaction.objects.filter(user=request.user)[:3]
             return render(
                 request,
                 "components/successful_transaction.html",
@@ -26,10 +26,22 @@ def home(request):
                 {"form": form},
             )
 
-    return render(request, "home.html", {"form": form, "transactions": transactions})
+    return render(request, "home.html", {"form": form})
 
 
 def currencies(request, input_clicked):
     return render(
         request, "components/currencies.html", {"input_clicked": input_clicked}
+    )
+
+
+def transactions(request):
+    transactions_list = Transaction.objects.filter(user=request.user)
+    paginator = Paginator(transactions_list, 3)
+    page_number = request.GET.get("page")
+    transactions = paginator.get_page(page_number)
+    return render(
+        request,
+        "components/transactions.html",
+        {"transactions": transactions},
     )
